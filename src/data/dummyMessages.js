d9844c8c146a7a76f5bf9342c711e4ebd43f7368
Tamara Ilic <tamara@ilicmcfarlane.io>
2022-01-14 21:19:25 +0000

    create folder structure and add dummy data

---
+++ src/data/dummyMessages.js
@@ -0,0 +1,38 @@
+export const dummyMessages = [
+    {
+        text: 'This is Abba Kyari the man in charge of the abduction and killing of Igbo youth in the SouthEast. He claims to be fighting insecurity but he is the cause of the insecurity!',
+        user: {
+            handle: '@AmBiafran'
+        },
+        date: 13/01/2022,
+        img_url: '',
+        link_url: 'https://t.co/rn7PqyjZqW'
+    },
+    {
+        text: 'This is Abba Kyari the man in charge of the abduction and killing of Igbo youth in the SouthEast. He claims to be fighting insecurity but he is the cause of the insecurity!',
+        user: {
+            handle: '@AmBiafran'
+        },
+        date: 13/01/2022,
+        img_url: '',
+        link_url: 'https://t.co/rn7PqyjZqW'
+    },
+    {
+        text: 'This is Abba Kyari the man in charge of the abduction and killing of Igbo youth in the SouthEast. He claims to be fighting insecurity but he is the cause of the insecurity!',
+        user: {
+            handle: '@AmBiafran'
+        },
+        date: 13/01/2022,
+        img_url: '',
+        link_url: 'https://t.co/rn7PqyjZqW'
+    },
+    {
+        text: 'This is Abba Kyari the man in charge of the abduction and killing of Igbo youth in the SouthEast. He claims to be fighting insecurity but he is the cause of the insecurity!',
+        user: {
+            handle: '@AmBiafran'
+        },
+        date: 13/01/2022,
+        img_url: '',
+        link_url: 'https://t.co/rn7PqyjZqW'
+    }
+]
---
+++ src/data/dummyUsers.js
@@ -0,0 +1,17 @@
+export const dummyUsers = [
+    {
+        name:'Medwin van der Shah',
+        profile_pic: 'https://twitter.com/DrMedShah/photo',
+        handle: '@DrMedShah'
+    },
+    {
+        name:'Medwin van der Shah',
+        profile_pic: 'https://twitter.com/DrMedShah/photo',
+        handle: '@DrMedShah'
+    },
+    {
+        name:'Medwin van der Shah',
+        profile_pic: 'https://twitter.com/DrMedShah/photo',
+        handle: '@DrMedShah'
+    }
+]
