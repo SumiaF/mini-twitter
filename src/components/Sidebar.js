d7270f4a6f0c5e20ca7221b6aebab0d7372899fe
Tamara Ilic <tamara@ilicmcfarlane.io>
2022-01-15 07:09:32 +0000

    simplify sidebar styling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,8 @@
 import "../styles/Sidebar.css"
-import { useState } from "react";
 
-export default function Sidebar({ name, profilePic, handle }) {
+export default function Sidebar() {
   return (
-    <aside className="sidebar-userinfo">
+    <aside className="sidebar">
        <ul>
         <li>
           <a>Home</a>
